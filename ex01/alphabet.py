9662a35b6e7dc31dc05cecd5868b721e906a09e1
Yuichiro <c0b210771b@edu.teu.ac.jp>
2022-09-27 17:42:57 +0900

    課題０１ #3

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -55,5 +55,4 @@ if __name__ == "__main__":
         else:
             print("-"*20)
     end_time = time.time()
-    time = end_time - start_time
-    print(f"[経過時間：{time:.2f}秒]")
+    print(f"[経過時間：{(end_time - start_time):.2f}秒]")
