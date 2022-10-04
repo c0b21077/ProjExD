c59c18d855741b9af067302d8a7324586470374a
Yuichiro <c0b210771b@edu.teu.ac.jp>
2022-10-04 17:51:50 +0900

    変更 #4

--- ex02/calc.py
+++ ex02/calc.py
@@ -30,7 +30,7 @@ entry.grid(row=0, column=0, columnspan=5)
 r = 1
 c = 0
 numbers = list(range(9, -1, -1))
-kigou = ["/","*","-","+"] 
+sign = ["/","*","-","+"] 
 
 for i in numbers:
     button = tk.Button(root, text=f"{i}", font=("Times New Roman",30), width="4", height="2")
@@ -41,7 +41,7 @@ for i in numbers:
         c = 0
         r += 1
 
-for i in kigou:
+for i in sign:
     button = tk.Button(root, text=f"{i}", font=("Times New Roman",30), width="4", height="2")
     button.bind("<1>", button_click)
     button.grid(row = r, column = 4)
