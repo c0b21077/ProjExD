fd557f6c052b379f12a5c245857681d5c2446545
Yuichiro <c0b210771b@edu.teu.ac.jp>
2022-10-04 17:20:04 +0900

    演習課題1

--- ex02/calc.py
+++ ex02/calc.py
@@ -30,7 +30,7 @@ entry.grid(row=0, column=0, columnspan=5)
 r = 1
 c = 0
 numbers = list(range(9, -1, -1))
-kigou = ["/","*","-","+"]
+kigou = ["/","*","-","+"] 
 
 for i in numbers:
     button = tk.Button(root, text=f"{i}", font=("Times New Roman",30), width="4", height="2")
