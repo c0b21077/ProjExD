733973b685773edad3e7b96820ae5c1a2c622f9f
Yuichiro <c0b210771b@edu.teu.ac.jp>
2022-10-04 17:13:51 +0900

    演習課題

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,20 +1,59 @@
-print("hello world")
-
 import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    entry.insert(tk.END, num)
+
+def click_equal(event):
+    eqn = entry.get()
+    ans = eval(eqn)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, ans)
+
+def click_C(event):
+    eqn = entry.get()
+    entry.delete(0, tk.END)
+    entry.insert(0,eqn[:-1])
+
+def click_AC(event):
+    entry.delete(0,tk.END)
 
 root = tk.Tk()
 root.title("電卓")
-root.geometry("300x500")
+root.geometry("400x600")
 
-r = 0
+entry = tk.Entry(root, width=8, font=(", 40"), justify="right")
+entry.grid(row=0, column=0, columnspan=5)
+
+r = 1
 c = 0
+numbers = list(range(9, -1, -1))
+kigou = ["/","*","-","+"]
 
-for i in range(9, -1, -1):
+for i in numbers:
     button = tk.Button(root, text=f"{i}", font=("Times New Roman",30), width="4", height="2")
+    button.bind("<1>", button_click)
     button.grid(row = r, column = c)
     c += 1
     if c == 3:
         c = 0
         r += 1
+
+for i in kigou:
+    button = tk.Button(root, text=f"{i}", font=("Times New Roman",30), width="4", height="2")
+    button.bind("<1>", button_click)
+    button.grid(row = r, column = 4)
+    r -= 1
+
+button = tk.Button(root, text="C/AC", font=("Times New Roman",30), width="4", height="2")
+button.bind("<1>", click_C)
+button.bind("<3>", click_AC)
+button.grid(row = 4, column = 1)
+
+button = tk.Button(root, text="=", font=("Times New Roman",30), bg = "PowderBlue", width="4", height="2")
+button.bind("<1>", click_equal)
+button.grid(row = 4, column = 2)
 
 root.mainloop()
