8045c95cc8db073d723fedfbfdaf22f9f0c16c49
Yuichiro <c0b210771b@edu.teu.ac.jp>
2022-10-04 14:26:38 +0900

    練習問題２

--- calc.py
+++ calc.py
@@ -3,6 +3,18 @@ print("hello world")
 import tkinter as tk
 
 root = tk.Tk()
+root.title("電卓")
 root.geometry("300x500")
+
+r = 0
+c = 0
+
+for i in range(9, -1, -1):
+    button = tk.Button(root, text=f"{i}", font=("Times New Roman",30), width="4", height="2")
+    button.grid(row = r, column = c)
+    c += 1
+    if c == 3:
+        c = 0
+        r += 1
 
 root.mainloop()
